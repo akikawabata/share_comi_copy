1ce716fee74862e57381441a6c8cc33d7e93b283
AkiKawabata <aki.iknh@gmail.com>
2024-07-16 10:55:05 +0000

    “memberモデルのデフォルト画像ファイル名のコード修正”

--- app/models/member.rb
+++ app/models/member.rb
@@ -19,7 +19,7 @@ class Member < ApplicationRecord
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/no_image.png')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
